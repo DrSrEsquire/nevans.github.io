896983811bf87b004a2f2a65fa42fe9970538e25
DrSrEsquire <sfcc.nicholas.evans@gmail.com>
2017-12-07 18:23:13 -0800

    changes

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+//Button Hover Effects
+
+$(function() {  
+  $('.btn-6')
+    .on('mouseenter', function(e) {
+			var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+			$(this).find('span').css({top:relY, left:relX})
+    })
+    .on('mouseout', function(e) {
+			var parentOffset = $(this).offset(),
+      		relX = e.pageX - parentOffset.left,
+      		relY = e.pageY - parentOffset.top;
+    	$(this).find('span').css({top:relY, left:relX})
+    });
+  $('[href=#]').click(function(){return false});
+});
+
+//Background Video
+$('#video').YTPlayer({
+  videoId: 'OZAfse9Pb4c'
+});
